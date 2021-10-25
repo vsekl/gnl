5228b3ae3be2956accfe002146a94324593d0ccc
vsekl <91872235+vsekl@users.noreply.github.com>
2021-10-25 18:31:48 +0300

    Add files via upload

---
+++ gnl/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vjose <vjose@student.21-school.ru>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 13:12:04 by vjose             #+#    #+#             */
+/*   Updated: 2021/10/19 13:12:04 by vjose            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "unistd.h"
+# include <stdlib.h>
+
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *s);
+void		*ft_memcpy(void	*dest, const void	*src, size_t n);
+char		*ft_strdup(const char *s);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+static char	*ft_check_remainder(char *remainder);
+static char	*ft_concatenation(char *remainder, char *line, char *buf);
+static char	*ft_return_line(char *remainder, char *line);
+static char	*ft_end_of_file(char *remainder, char *line, char *buf);
+
+#endif
